ce4b4a740e6f6516040713abf367d9d2f5aca44d
Olivia Dydzuhn <dydzuhnolivia@gmail.com>
2021-07-23 10:09:25 -0400

    hi

--- kwkfinal/SecondViewController.swift
+++ kwkfinal/SecondViewController.swift
@@ -1,35 +1,34 @@
 //
-//  secondViewController.swift
-//  kwkfinal
+//  ViewController.swift
+//  8alance Project
 //
 //  Created by  Scholar on 7/21/21.
 //
-
 import UIKit
 
 class secondViewController: UIViewController {
-    
     var pick = 1
 
     @IBOutlet weak var blankLabel: UILabel!
-    
     @IBAction func homeOneViewButtonPressed(_ sender: AnyObject) {
         print("home button pressed :)")
         self.performSegue(withIdentifier: "HomeOneViewSegue", sender: self)
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-        override func becomeFirstResponder() -> Bool {
+    override func becomeFirstResponder() -> Bool {
         return true
-       }
-        override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-               if motion == .motionShake
-    {
+        
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake
+        {
             if pick == 1
         {
-            blankLabel.text = "Meditate"
-        }
+                blankLabel.text = "Meditate"
+            }
         else
             if pick == 2
         {
@@ -37,51 +36,50 @@ class secondViewController: UIViewController {
             }
         else
             if pick == 3
-        {
-                blankLabel.text = "Write in a journal"
-            }
+    {
+        blankLabel.text = "Write in a journal"
+        }
         else
             if pick == 4
-        {
-                blankLabel.text = "List five things you're grateful for"
-            }
-        else
+    {
+            blankLabel.text = "Face Mask"
+        }
+           else
             if pick == 5
-        {
-                blankLabel.text = "Exercise"
-            }
-        else
+    {
+                blankLabel.text = "Paint your nails"
+        }
+            else
             if pick == 6
-        {
-                blankLabel.text = "Sing your favorite song"
-            }
-        else
+    {
+                blankLabel.text = "Light a candle"
+        }
+            else
             if pick == 7
-        {
-                blankLabel.text = "Go on a walk"
-            }
-        else
+    {
+                blankLabel.text = "Exersize"
+        }
+            else
             if pick == 8
-        {
-                blankLabel.text = "Do cardio"
-            }
-        else
+    {
+                blankLabel.text = "Listen to music"
+        }
+            else
             if pick == 9
-        {
-                blankLabel.text = "Go out"
-            }
-        else
+    {
+                blankLabel.text = "Cook a meal"
+        }
+            else
             if pick == 10
-        {
-                blankLabel.text = "Go for a run"
-            }
-                pick = pick + 1
+    {
+                blankLabel.text = "Take a bath"
+        }
+            pick = pick + 1
         }
     }
-       func myRand(a: Int, b: Int) -> Int{
-           let range = b-a
-           let rand = Int(arc4random_uniform(UInt32(range)))+a
-           return rand
+    func myRand(a: Int, b: Int) -> Int{
+        let range = b-a
+        let rand = Int(arc4random_uniform(UInt32(range)))+a
+        return rand
     }
-
 }
